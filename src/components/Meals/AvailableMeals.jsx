9c041a557a69b60197c7a45cc287837cf321e97f
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-02-08 17:41:58 -0500

    Create AvailableMeals component and css

---
+++ src/components/Meals/AvailableMeals.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import classes from './AvailableMeals.module.css'
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Italian Sausage with the works',
+    description: 'Sausages made from organic, healthily raised pigs that are processed in the most humane ways available.',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Corn on the Cob',
+    description: 'Juicy, mouth-watering Corn on the Cob made same day and picked by your Canadian farmers.',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Double-Decker Grilled Cheese Burger',
+    description: '2 juicy 6oz patties between 2 grilled cheese sandwhiches instead of buns.',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Soup',
+    description: 'Mixture of al the organic vegetables we could find... Yum!',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map(meal => {
+    <li>{meal.name}</li>
+  })
+  return (
+    <section className={classes.meal}>
+      <ul>
+        {mealsList}
+      </ul>
+    </section>
+  )
+}
+
+export default AvailableMeals
