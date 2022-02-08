f0ab8c27edc772a620f7c7c08212610a317bb16a
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-02-08 16:08:16 -0500

    Add Cart Button to Header Component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import Hero from "../../assets/Food-Truck-Hero.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>Food Truck</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={Hero} alt="Buffet of Delicious Food on a table" />
