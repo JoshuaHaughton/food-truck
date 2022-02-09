8d1e30257af6116bee0ff77eb2ce23fb05e1e13e
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-02-09 11:01:54 -0500

    Initialize useReducer in the CartProvider (Context) component

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+
+
+
+
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+//Shouldn't be recreated everytime the component is reevaluated, so we put outside of Component
+const cartReducer = (state, action) => {
+ 
+  return defaultCartState
+}
 
 //Allows for wrapping of any components that need to get access to this context
 const CartProvider = (props) => {
+  //Using useReducer instead of useState because state is complex in this component
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
 
-  const addCartItem = item => {}
+  const addCartItem = item => {
+    dispatchCartAction({type: 'ADD', item: item});
+  }
 
 
   const removeCartItem = id => {}
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addCartItem,
     removeItem: removeCartItem
   }
