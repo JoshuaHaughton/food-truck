cbe81e757c2c69457831cf073c6a5910cb55fbf6
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-02-09 19:45:18 -0500

    Add functionality to add to the item count in Context if item being added already exists in the items array

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -13,10 +13,30 @@ const defaultCartState = {
 //Shouldn't be recreated everytime the component is reevaluated, so we put outside of Component
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    //Concat returns a new array instead of mutating the current one like push
-    const updatedItems = state.items.concat(action.item);
-    //existing??
+    //New total amount
     const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+    
+    // if item already exists in cart
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      //update that item and the items in Context,
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      //else, just add it to the array
+      updatedItems = state.items.concat(action.item)
+    }
+
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
@@ -35,7 +55,7 @@ const CartProvider = (props) => {
     dispatchCartAction({type: 'ADD', item: item});
   }
 
-  
+
 
   const removeCartItem = id => {}
 
